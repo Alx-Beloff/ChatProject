ecfe7618bfdff5f60b93853b6cb20f386e7118bf
Alx-Beloff <alx.beloff@mail.ru>
2024-05-02 11:16:59 +0300

    initial commit

---
+++ server/app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const config = require('./config/serverConfig');
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(cors({ credentials: true, origin: true }));
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const indexRouter = require('./routes/index.routes');
+
+config(app);
+
+app.use('/', indexRouter);
+
+app.listen(PORT, () => {
+  console.log(`App has been started in port ${PORT}...`);
+});

64ebc9fe07e495f8710fd8a54382c898deacabab
Alx-Beloff <alx.beloff@mail.ru>
2024-05-13 17:51:58 +0300

    chat romm add

--- client/src/components/pages/ChatPage.tsx
+++ client/src/components/pages/ChatPage.tsx
@@ -1,26 +1,32 @@
 import React, { useEffect, useRef } from 'react';
-import { Card, Col, Container, Row } from 'react-bootstrap';
-import UsersLists from '../ui/UsersLists';
+import { Container } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 import ChatComponent from '../ui/ChatComponent';
-import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { useAppDispatch } from '../../redux/hooks';
 import { setMessages, setUsers } from '../../redux/slices/messages/messagesSlice';
 import AppNavbar from '../ui/AppNavbar';
 
 export default function ChatPage(): JSX.Element {
   const socketRef = useRef(null);
   const dispatch = useAppDispatch();
+  const { spotId } = useParams();
 
   useEffect(() => {
-    socketRef.current = new WebSocket('ws://localhost:3001');
+    socketRef.current = new WebSocket(`${import.meta.env.VITE_WS_URL}/?spotId=${spotId}`);
     const socket = socketRef.current;
     socket.onmessage = (event: MessageEvent) => {
       const { type, payload } = JSON.parse(event.data);
+
       switch (type) {
         case 'SET_USERS_FROM_SERVER':
           dispatch(setUsers(payload));
           break;
 
         case 'ADD_MESSAGE_FROM_SERVER':
+          dispatch(setMessages(payload));
+          break;
+
+        case 'SET_HISTORY_FROM_SERVER':
           dispatch(setMessages(payload));
           break;
 
@@ -37,7 +43,10 @@ export default function ChatPage(): JSX.Element {
     }
   };
   return (
-    <Container style={{ height: '100vh', display: 'flex', flexDirection: 'column' }} className="col-lg-8 col-xl-7 p-0">
+    <Container
+      style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}
+      className="col-lg-8 col-xl-7 p-0"
+    >
       <AppNavbar />
       <ChatComponent submitMessage={submitMessage} socketRef={socketRef} />
     </Container>
